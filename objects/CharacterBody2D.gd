b78456f3159c96fbe437beb554a494a2c344d4b0
WatDuhHekBro <watduhhekbro@protonmail.com>
2024-03-11 14:46:54 -0500

    Fixed isometric movement

--- objects/CharacterBody2D.gd
+++ objects/CharacterBody2D.gd
@@ -8,23 +8,20 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction_x = Input.get_axis("ui_left", "ui_right")
-	var direction_y = Input.get_axis("ui_up", "ui_down")
+	var direction_y = Input.get_axis("ui_down", "ui_up")
 	
 	if direction_x:
 		velocity.x = direction_x * SPEED * cos(deg_to_rad(30))
 		velocity.y = direction_x * SPEED * sin(deg_to_rad(30))
-		print(cos(deg_to_rad(30)), sin(deg_to_rad(30)))
+		print('x\' = ', velocity.x)
+		print('y\' = ', velocity.y)
+	elif direction_y:
+		velocity.x = direction_y * SPEED * cos(deg_to_rad(-30))
+		velocity.y = direction_y * SPEED * sin(deg_to_rad(-30))
 		print('x = ', velocity.x)
 		print('y = ', velocity.y)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	if direction_y:
-		velocity.x = direction_y * SPEED * cos(deg_to_rad(30))
-		velocity.y = direction_y * SPEED * sin(deg_to_rad(30))
-		print('x = ', velocity.x)
-		print('y = ', velocity.y)
-	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()

f8debfe44ba10a694b6448f435a6aaa75989e7b9
WatDuhHekBro <watduhhekbro@protonmail.com>
2024-03-11 14:37:23 -0500

    Isometric movement test

---
+++ objects/CharacterBody2D.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction_x = Input.get_axis("ui_left", "ui_right")
+	var direction_y = Input.get_axis("ui_up", "ui_down")
+	
+	if direction_x:
+		velocity.x = direction_x * SPEED * cos(deg_to_rad(30))
+		velocity.y = direction_x * SPEED * sin(deg_to_rad(30))
+		print(cos(deg_to_rad(30)), sin(deg_to_rad(30)))
+		print('x = ', velocity.x)
+		print('y = ', velocity.y)
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if direction_y:
+		velocity.x = direction_y * SPEED * cos(deg_to_rad(30))
+		velocity.y = direction_y * SPEED * sin(deg_to_rad(30))
+		print('x = ', velocity.x)
+		print('y = ', velocity.y)
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()

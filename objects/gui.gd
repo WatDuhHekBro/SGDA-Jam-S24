c83b20eec9054dc7215bdf19b73ec02389503414
WatDuhHekBro <watduhhekbro@protonmail.com>
2024-03-15 12:51:25 -0500

    Finished game over screen

--- objects/gui.gd
+++ objects/gui.gd
@@ -2,9 +2,24 @@ extends CanvasLayer
 
 
 @onready var label = $GUI/Label
+@onready var gameover = $GUI/GameOver
+@onready var timer = $Timer
+var is_gameover = false
 
 
 func update_text(wallphase_count: int, has_wallrun: bool, has_timejump: bool):
 	label.text = "Dash Count: %s
 Wallrun Available? %s
 Timejump Available? %s" % [wallphase_count, "Yes" if has_wallrun else "No", "Yes" if has_timejump else "No"]
+
+
+func set_game_over(status: bool):
+	is_gameover = status
+	
+	if status:
+		timer.start()
+		gameover.visible = true
+
+
+func _on_timer_timeout():
+	gameover.visible = !gameover.visible

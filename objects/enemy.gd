893a26855cac6370442a924ae123b035a8137417
Rodrigues Rodriguez <dar220007@utdallas.edu>
2024-03-16 20:59:17 -0500

    line of sight for enemies

--- objects/enemy.gd
+++ objects/enemy.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed: float = 50.0
+@export var speed: float = 40.0
 @export var is_aggressive = false
 @onready var nav: NavigationAgent2D = $NavigationAgent2D
 @onready var target = $%Player
@@ -13,7 +13,7 @@ func _ready() -> void:
 func _physics_process(_delta):
 	if nav.is_navigation_finished():
 		_on_velocity_computed(Vector2(0, 0))
-	if is_seeking:
+	if is_seeking && is_line_of_sight():
 		target_last_seen = target.global_position
 		nav.set_target_position(target_last_seen)
 
@@ -21,22 +21,25 @@ func _physics_process(_delta):
 		var new_velocity = global_position.direction_to(nav.get_next_path_position()) * speed
 		_on_velocity_computed(new_velocity)
 
+func is_line_of_sight() -> bool:
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(global_position, target.global_position)
+	var result = space_state.intersect_ray(query)
+	if result:
+		return result.collider == target
+	return false
 
-	move_and_slide()	
 
 func _on_velocity_computed(safe_velocity: Vector2):
 	velocity = safe_velocity
 	move_and_slide()
 
 func _on_player_entered(body):
-	print("SEEK TRUE")
 	is_seeking = true
 
 func _on_player_exited(body):
-	print("SEEK FALSE")
 	is_seeking = false
 
 func _on_kill_player_entered(body: Node2D):
-	print("KILLING PLAYER")
 	$%Player.kill()
 

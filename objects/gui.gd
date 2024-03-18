93de0e90138562c2968ca65fe9f3dbcf0a04da1c
Rodrigues Rodriguez <dar220007@utdallas.edu>
2024-03-18 00:50:41 -0500

    final changes

--- objects/gui.gd
+++ objects/gui.gd
@@ -5,6 +5,7 @@ extends CanvasLayer
 @onready var wallrun_status = $Hotbar/Wallrun/On
 @onready var timejump_status = $Hotbar/Timejump/On
 @onready var gameover = $GameOver
+@onready var pause_menu = $PausedGame
 var is_gameover = false
 
 func _ready():
@@ -21,7 +22,21 @@ func set_gameover(status: bool):
 	is_gameover = status
 	gameover.visible = status
 
-
 func _input(event):
 	if event.is_action_pressed("menu-start") and is_gameover:
 		get_tree().reload_current_scene()
+
+
+func _on_button_pressed():
+	get_tree().paused = true
+	pause_menu.visible = true
+
+
+func _on_resume_pressed():
+	get_tree().paused = false
+	pause_menu.visible = false
+
+func _on_main_menu_pressed():
+	get_tree().paused = false
+	pause_menu.visible = false
+	get_tree().change_scene_to_file("res://maps/offgameplay/menu.tscn")
